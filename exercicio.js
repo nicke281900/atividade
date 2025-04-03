eb298fabe7afca07d3e8d0d7f2e212d8885dc625
nicke281900 <126591579+nicke281900@users.noreply.github.com>
2025-04-03 09:57:58 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,42 @@
+// 1. Exiba a mensagem"ola mundo"! no console.
+console.log("ola,mundo!");
+
+// 2. Cria uma variavel com o seu nome e exiba no console.
+let nome = "NIcole s";
+console.log(nome)
+
+// 3. Some dois numeros e exiba no console.
+let num1 = 17;
+let num2 = 1;
+console.log(num1 + num2);
+
+// 4. Subtraia dois numeros e exiba o resultado.
+console.log(num1 - num2);
+
+// 5. Multiplique dois numeros e exiba o resultado.
+console.log(num1 * num2);
+
+// 6. Divida dois numeros e exiba o resultado.
+console.log(num1 / num2);
+
+// 7. Descubra o resto da divisao de um numero para o outro.
+console.log(num1 % num2);
+
+// 8. Crie uma variável booleana (true ou false) exiba seu valor.
+let éMaiora = true;
+console.log(éMaior);
+
+// 9. Verifique se um número é maior que outro eexiba o resultado.
+console.log(num1 > num2);
+
+// 10. Junte duas palavras (strings) e exiba o resultado.
+let palavra1 ="Nicole";
+let palavra2 = "s";
+console.log(palavra1 + palavra2);
+
+// 11. Converta uma string que representa um númeropara um tipo numérico.
+let stringsNumero = "23"
+let numeroConvertido = Number(stringsNumero);
+console.log(numeroConvertido);
+
+// 12. Verifique se um número é par ou ímpar.
